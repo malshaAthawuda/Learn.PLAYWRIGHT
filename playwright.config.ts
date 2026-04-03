32144455182abdc53735d5cc016028e3ceb36654
Malsha Athawuda <malshaathawuda2003@gmail.com>
2026-04-03 13:00:52 +0530

    Update test configuration to capture screenshots on first failure and enable video recording

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,6 +15,8 @@ const config :PlaywrightTestConfig= {
   testMatch : ["tests/Recorded.test.ts"],
   use: {
     headless :false,
+     screenshot : "only-on-failure",  //add screenshot and video to the report
+     video : "retain-on-failure",
   },
   reporter: [["dot"],["json", {
     outputFile: "jsonReports/jsonReport.json" //1 . folder name and then 2. file name 
